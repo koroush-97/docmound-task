c88dfd73ed9ea0acd6e3e9bb88104c17ae474f96
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-01 08:16:04 +0330

    project ready

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default function DashboardPage() {
 
   return (
     <div className={`${styles.dashboardContainer}  flex flex-row`}>
-      <div className="content w-3/4 h-full  flex flex-col">
+      <div className="content w-3/4 h-full hidden  md:flex flex-col">
         <div className="border-2 h-1/3 flex justify-center items-center">
           <h1 className="text-4xl direction-rtl">
             {" "}
@@ -33,7 +33,12 @@ export default function DashboardPage() {
           داشبورد
         </div>
       </div>
-      <div className="sidebar w-1/4 h-full">
+      <div className="sidebar w-full md:w-1/4 h-full">
+        <div className=" md:hidden flex p-2 justify-center items-center border-2">
+          <h2>
+            خوش آمدید <span>{parsedUser.name.first}</span> به پنل کاربری
+          </h2>
+        </div>
         <div className={`${styles.imgSidebar}`}>
           <img src={parsedUser.picture.medium} alt="avatar" />
         </div>
